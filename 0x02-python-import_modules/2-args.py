6ed33fbc6fe23fe7cda5bf863a4c75b1aa2d5d26
dericko681 <abonghoderick@gmail.com>
2024-01-10 10:55:57 -0800

    task files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
-    else:
-        print("{} arguments:".format(i))
-        if i >= 1:
-            i = 0
-            for arg in sys.argv:
-                if i != 0:
-                    print("{}: {}".format(i, arg))
-                    i += 1
 
+from sys import argv
+
+if __name__ == "__main__":
+
+    num_a = len(argv) - 1
+
+    if num_a == 0:
+        print("0 arguments.")
+    elif num_a == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_a))
+
+    for d, j in enumerate(argv[1:]):
+        print("{}: {}".format(d + 1, j))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
 
+from sys import argv
+
+if __name__ == "__main__":
+
+    print(sum(map(int, argv[1:])))
